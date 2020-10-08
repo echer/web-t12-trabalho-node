c2db0b4f42635329fffa1f109ffd9b4ef7f8da4f
alan echer <alan.echer@gmail.com>
2020-10-08 14:59:10 -0300

    401 messages

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,21 +1,24 @@
-module.exports = app => {
-    app.use((req, res, next) => {
-        if(req.originalUrl == "/usuarios/login" || (req.originalUrl == "/usuarios" && req.method == "POST")){
-            next()
-        } else {
-            let token = req.headers.token
-            if(!token)
-                res.status(401).send("Faltou enviar o token")
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    if (
+      req.originalUrl == "/usuarios/login" ||
+      (req.originalUrl == "/usuarios" && req.method == "POST")
+    ) {
+      next();
+    } else {
+      let token = req.headers.token;
+      if (!token) res.status(401).send("Não autorizado");
+      else {
+        app
+          .get("jwt")
+          .verify(token, process.env.JWT_CHAVE_PRIVADA, (err, decoded) => {
+            if (err) res.status(401).send("Não autorizado - Token inválido");
             else {
-                app.get("jwt").verify(token, process.env.JWT_CHAVE_PRIVADA, (err, decoded) => {
-                    if(err)
-                        res.status(401).send("Token inválido")
-                    else { 
-                        req.decoded = decoded
-                        next();
-                    }
-                })
+              req.decoded = decoded;
+              next();
             }
-        }
-    })
-}
+          });
+      }
+    }
+  });
+};
